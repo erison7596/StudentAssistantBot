85536d3ab001ca316ce97f6fe113dd040e7238da
Erison <erison7596e@gmail.com>
2024-06-26 03:01:12 -0300

    iniciado treinamento

---
+++ actions/actions.py
@@ -0,0 +1,20 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionGreetUser(Action):
+    def name(self) -> Text:
+        return "action_greet_user"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        name = tracker.get_slot('name')
+        if not name:
+            dispatcher.utter_message(text="Oi, Eu sou o BotIF. Como posso ajudar você hoje?")
+        else:
+            dispatcher.utter_message(text=f"Oi {name}, Eu sou o BotIF. Como posso ajudar você hoje?")
+        return []
